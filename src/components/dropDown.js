192d8adbb828b7df28c00b5e9721d6b49f090330
Jack Yeung <jyeung.se@gmail.com>
2019-03-20 23:32:24 -0400

    Created initial shell files for components.

---
+++ src/components/dropDown.js
@@ -0,0 +1,17 @@
+import React { Component } from 'react'
+
+class DropDown extends Component {
+  constructor(props){
+    super(props)
+
+    this.state={
+
+    }
+  }
+
+  render(){
+    return (
+
+    )
+  }
+}
---
+++ src/components/tracker.js
@@ -0,0 +1,8 @@
+
+
+const Tracker = props => {
+
+  return (
+
+  )
+}
